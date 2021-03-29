9e4474bb173c0b9f7610cc37722d597d9ad7d2f9
Priti Yadav <pritiyadav888@gmail.com>
2021-03-29 11:59:08 -0700

    updated mongodb url to localhost

--- server/config/db.js
+++ server/config/db.js
@@ -5,7 +5,5 @@
 module.exports =
 {
     "URI": "mongodb://localhost/surveys"
-    // "URI": "mongodb+srv://new_priti:DnJJKkw1rdsuk6TJ@booklist.6ozgq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-//     "URI": "mongodb+srv://new_priti:DnJJKkw1rdsuk6TJ@booklist.6ozgq.mongodb.net/bookList?retryWrites=true&w=majority"
-// 
+   
 }
