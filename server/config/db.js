a3ba98f6426a5a73d89aa78c1ffa1f501f63b341
Priti Yadav <pritiyadav888@gmail.com>
2021-04-04 15:21:49 -0700

    Changed URI for mongoDB cloud and added pictures to about page

--- server/config/db.js
+++ server/config/db.js
@@ -4,7 +4,6 @@
  */
 module.exports =
 {
-    "URI": "mongodb://localhost/surveys"
+    "URI": "mongodb+srv://priti:EZfIzdfbedekn8Ig@survey.pk8lj.mongodb.net/surveys?retryWrites=true&w=majority"
    
 }
-    
